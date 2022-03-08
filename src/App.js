b4c92b94245de2983d13a54ccb137e138f1c5224
shota donguzashvili <shota.nope@gmail.com>
2022-03-08 20:24:36 +0400

    fixed host

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import './App.css';
 import WelcomePage from './Components/WelcomePage/welcome';
 import Questions from './Components/Questions/Questions';
 import Applications from './Components/Applications/Applications';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<WelcomePage />} />
           <Route path="/questions/:page" element={<Questions />} />
           <Route path="/applications" element={<Applications />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
